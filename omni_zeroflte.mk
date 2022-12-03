ea86a2a004b4620b5baf89585cd8554df2e176d1
derveror <58082462+derveror@users.noreply.github.com>
2022-12-03 03:49:59 +0300

    Update omni_zeroflte.mk

--- omni_zeroflte.mk
+++ omni_zeroflte.mk
@@ -1,13 +1,31 @@
-# Release name
-PRODUCT_RELEASE_NAME := zeroflte
+#
+# Copyright (C) 2022 The OrangeFox Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-$(call inherit-product, build/target/product/embedded.mk)
+# Inherit from the common Open Source product configuration
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from this product for devices that support only 64-bit apps using:
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := zeroflte
 PRODUCT_NAME := omni_zeroflte
-PRODUCT_BRAND := samsung
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := SM-G920F
 PRODUCT_MANUFACTURER := samsung
